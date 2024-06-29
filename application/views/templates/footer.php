7799f9461f227ed2cd9c90dcfbdc8f64bad67911
mahito <priambodoputut557@gmail.com>
2024-06-30 01:57:28 +0700

    Update crud codeigniter3 ( 2 )

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -33,6 +33,19 @@
 					$('#show_hide_password2 i').addClass("bx-show");
 				}
 			});
+
+			$("#show_hide_password a").on('click', function (event) {
+				event.preventDefault();
+				if ($('#show_hide_password input').attr("type") == "text") {
+					$('#show_hide_password input').attr('type', 'password');
+					$('#show_hide_password i').addClass("bx-hide");
+					$('#show_hide_password i').removeClass("bx-show");
+				} else if ($('#show_hide_password input').attr("type") == "password") {
+					$('#show_hide_password input').attr('type', 'text');
+					$('#show_hide_password i').removeClass("bx-hide");
+					$('#show_hide_password i').addClass("bx-show");
+				}
+			});
 		});
 	</script>
 	<!--app JS-->
